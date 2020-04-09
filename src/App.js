33cf728268d2d4cb56a17562ad70c2789dc9dcea
Avram Eisner <avrame@gmail.com>
2020-04-09 11:44:07 -0700

    Skeleton app created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Overview from './pages/overview';
+import Sales from './pages/sales';
 import './App.css';
 
+const OVERVIEW_PAGE = 'OVERVIEW_PAGE';
+const SALES_PAGE = 'SALES_PAGE';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ page, setPage ] = useState(OVERVIEW_PAGE);
+
+	let pageComponent;
+	switch (page) {
+		case OVERVIEW_PAGE:
+			pageComponent = <Overview />;
+			break;
+		case SALES_PAGE:
+			pageComponent = <Sales />;
+			break;
+		default:
+			pageComponent = <Overview />;
+	}
+
+	return (
+		<div className="App">
+			<header>
+				<img className="logo" src="logo-white.svg" alt="Stackline Logo" />
+			</header>
+			<div class="wrapper">
+				<div className="side">
+					<div className="product-info" />
+					<div className="tags" />
+					<nav>
+						<a href="#overview" onClick={() => setPage(OVERVIEW_PAGE)}>
+							Overview
+						</a>
+						<a href="#sales" onClick={() => setPage(SALES_PAGE)}>
+							Sales
+						</a>
+					</nav>
+				</div>
+
+				{pageComponent}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/sales.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function() {
+	return (
+		<div className="sales-page">
+			<div>
+				<h2>Retail Sales</h2>
+				<p>Graph goes here</p>
+			</div>
+			<div>
+				<table>
+					<thead>
+						<tr>
+							<th>Week Ending</th>
+							<th>Retail Sales</th>
+							<th>Wholesale Sales</th>
+							<th>Units Sold</th>
+							<th>Retailer Margin</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Sales</td>
+							<td>Data</td>
+							<td>Goes</td>
+							<td>Here</td>
+							<td>!!!!</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+}
